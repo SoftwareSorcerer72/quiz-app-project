303021d82a3eb43c2d3c43e88b608b493e1c0619
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-23 23:02:32 -0400

    Set up questions type

---
+++ src/QuestionsView.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+// Define a type for the question
+type Question = {
+    id: string;
+    question: string;
+    options: string[];
+    answer: string;
+};
+
+const QuestionsView = () => {
+    cinst [Questions, setQuestions] = useState<Questions[]>([]);
+
+
+}
