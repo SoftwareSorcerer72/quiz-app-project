b9a8de8dcb936343c370c3a92a3ea1c27e51b036
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-24 00:02:20 -0400

    Set up basic routes in App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// src/App.tsx
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import QuestionsView from './QuestionsView';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/questions" element={<QuestionsView />} />
+                <Route path="/" element={<div>Welcome to Quiz Up!</div>} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
